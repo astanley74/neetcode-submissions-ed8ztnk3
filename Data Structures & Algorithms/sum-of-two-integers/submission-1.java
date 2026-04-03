b41d9664f184e3da2013e23879768a02addd6968
Austin Stanley <66804122+astanley74@users.noreply.github.com>
2026-04-02 22:52:14 -0400

    Add: sum-of-two-integers - submission-1

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int getSum(int a, int b) {
+        int sum = 0, carry = 0;
+
+        for (int i = 0; i < 32; i++) {
+            int bit1 = (a >> i) & 1;
+            int bit2 = (b >> i) & 1;
+            int curr = bit1 ^ bit2 ^ carry;
+            carry = (bit1 + bit2 + carry) >= 2 ? 1 : 0;
+            if (curr != 0) {
+                sum |= (1 << i);
+            }
+        }
+        return sum;
+    }
+}
