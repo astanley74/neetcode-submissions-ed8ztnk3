243a4aa554065c550041f3fc24e5785bf72d378e
Austin Stanley <66804122+astanley74@users.noreply.github.com>
2026-04-17 23:23:55 -0400

    Add: min-cost-climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-1.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int[] cache = new int[cost.length + 1];
+
+        for (int i = 2; i <= cost.length; i++) {
+            cache[i] = Math.min(cache[i - 1] + cost[i - 1], cache[i - 2] + cost[i - 2]);
+        }
+        return cache[cost.length];
+    }
+}
