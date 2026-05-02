a9eba3e06ba2c3aa804e2d599a2d2ff7fbaf9c0b
Austin Stanley <66804122+astanley74@users.noreply.github.com>
2026-05-01 23:21:04 -0400

    Add: partition-equal-subset-sum - submission-1

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-1.java
@@ -0,0 +1,32 @@
+class Solution {
+    private Boolean[][] cache;
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        cache = new Boolean[nums.length][sum / 2 + 1];
+
+        return dfs(nums, 0, sum / 2);
+    }
+
+    private boolean dfs(int[] nums, int index, int target) {
+        if (index == nums.length) {
+            return target == 0;
+        }
+        if (target < 0) {
+            return false;
+        }
+
+        if (cache[index][target] != null) {
+            return cache[index][target];
+        }
+
+        cache[index][target] = dfs(nums, index + 1, target) || dfs(nums, index + 1, target - nums[index]);
+
+        return cache[index][target];
+    }
+}
