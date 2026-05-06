9f6802de9075f15dd7e8bf2aa89ed7cec9c5c808
Austin Stanley <66804122+astanley74@users.noreply.github.com>
2026-05-05 22:01:05 -0400

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int jump(int[] nums) {
+        int result = 0, curEnd = 0, curFar = 0;
+
+        for (int i = 0; i < nums.length - 1; ++i) {
+            curFar = Math.max(curFar, i + nums[i]);
+
+            if (i == curEnd) {
+                result++;
+                curEnd = curFar;
+            }
+        }
+
+        return result;
+    }
+}
